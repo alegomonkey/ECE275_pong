600ce28a6fd22273b3df29197436be8f6de2822a
alegomonkey <74006840+alegomonkey@users.noreply.github.com>
2025-04-29 15:40:25 -0400

    Pong ball move

--- VGA_video/VGA_videotop.sv
+++ VGA_video/VGA_videotop.sv
@@ -48,12 +48,31 @@ make_box draw_player_1_paddle(
 	.box(player_1_paddle)
 );	
 
+//Draw player 2 paddle
+wire [9:0] player_2_paddle_width=5;
+wire [9:0] player_2_paddle_height=50;
+wire [9:0] player_2_paddle_X_location=470;
+wire [9:0] player_2_paddle_Y_location=0;
+reg player_2_paddle;				
+make_box draw_player_2_paddle(
+	.X_pix(X_pix),
+	.Y_pix(Y_pix),
+	.box_width(player_2_paddle_width),
+	.box_height(player_2_paddle_height),
+	.box_X_location(player_2_paddle_X_location),
+	.box_Y_location(player_2_paddle_Y_location),
+	.pixel_clk(pixel_clk),
+	.box(player_2_paddle)
+);
+
 //Draw ball
 wire [9:0] ball_width=4;
 wire [9:0] ball_height=4;
-reg [9:0] ball_X_location=0;
-reg [9:0] ball_Y_location=0;
+reg signed [9:0] ball_X_location=230;
+reg signed [9:0] ball_Y_location=320;
 reg b_direction = 1;
+reg signed [4:0] b_velocity_X = -1;
+reg signed [4:0] b_velocity_Y = 1;
 reg ball;
 make_box draw_ball(
 	.X_pix(X_pix),
@@ -88,28 +107,52 @@ always @(posedge pixel_clk)
 		
 		if (ball_speed_counter == 200_000) begin
 		
+<<<<<<< Updated upstream
+=======
+		if (ball_speed_counter == 250_000) begin
+>>>>>>> Stashed changes
 			ball_speed_counter <= 0;
-			if (b_direction == 1 && ball_X_location > 5)
-				ball_X_location = ball_X_location - 1;
 			
-			else if (ball_X_location <= 5) begin
-				b_direction = 2;
-				ball_X_location = ball_X_location + 1;
-				end
-			
-			else if (ball_X_location == 640) begin
-				b_direction = 1;
-				ball_X_location = ball_X_location - 1;
-				end
-			
-			else
-				ball_X_location = ball_X_location + 1;
+			if (ball_X_location-1 == player_1_paddle) begin
+				ball_X_location = ball_X_location + 2;
+				b_velocity_X = -b_velocity_X;
+			end
+			else if (ball_X_location+1 == player_2_paddle) begin
+				ball_X_location = ball_X_location - 2;
+				b_velocity_X = -b_velocity_X;
+			end
+			else if (ball_X_location+1 >= 635)begin
+				ball_X_location = ball_X_location - 5;
+				b_velocity_X = -b_velocity_X;
+			end
+			else if (ball_X_location-1 <= 5)begin
+				ball_X_location = ball_X_location + 5;
+				b_velocity_X = -b_velocity_X;
+			end
+			//Y
+			else if(ball_Y_location-1 <= 5)begin
+				ball_Y_location = ball_Y_location + 5 ;
+				b_velocity_Y = -b_velocity_Y; 
+			end
+			else if(ball_Y_location+1 >= 470)begin
+				ball_Y_location = ball_Y_location - 5;
+				b_velocity_Y = -b_velocity_Y; 
+			end
+			else begin
+				ball_X_location = ball_X_location + b_velocity_X;
+				ball_Y_location = ball_Y_location + b_velocity_Y;
+			end
 		end
 		
+<<<<<<< Updated upstream
 		
 		
 		
 		if(player_1_paddle) pixel_color <= 12'b0000_1111_0000;
+=======
+		if(player_1_paddle) pixel_color <= 12'b0000_0000_1111;
+		else if(player_2_paddle) pixel_color <= 12'b1111_0000_0000;
+>>>>>>> Stashed changes
 		else if(ball) pixel_color <= 12'b1111_1111_1111;
 		else pixel_color <= 12'b0000_0000_0000;
 	end
